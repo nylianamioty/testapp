fe146ee50014b8d21824d2bcbd514210f15532c0
nylianamioty <miotyrazafimahefa@gmail.com>
2025-11-16 16:47:29 +0300

    test sprint4 bis

--- src/main/java/com/testapp/controllers/TestController.java
+++ src/main/java/com/testapp/controllers/TestController.java
@@ -1,23 +1,39 @@
 package com.testapp.controllers;
 
 import com.monframework.annotation.URLMapping;
+import com.monframework.mvc.ModelView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-// Utilise l'annotation avec le nom complet
 @com.monframework.annotation.Controller  
 public class TestController extends com.monframework.controller.Controller {
     
     @URLMapping("/hello")
     public String hello() {
-        return "<h1>Bonjour depuis le framework!</h1>";
-    }
-    
-    @URLMapping("/users/{id}")
-    public String getUser(String id) {
-        return "<p>Utilisateur ID: " + id + "</p>";
+        return "<h1>Bonjour depuis le framework!</h1><p>Ceci est un retour String direct</p>";
     }
     
     @URLMapping("/home")
-    public String home() {
-        return "<html><body><h1>Page d'accueil</h1><p>Bienvenue!</p></body></html>";
+    public ModelView home() {
+        ModelView mv = new ModelView();
+        mv.setView("test.jsp");
+        mv.addObject("message", "Bienvenue sur la page d'accueil!");
+        mv.addObject("users", new String[]{"Jean", "Marie", "Paul"});
+        return mv;
+    }
+    
+    @URLMapping("/users/{id}")
+    public ModelView getUser(String id) {
+        ModelView mv = new ModelView();
+        mv.setView("user.jsp");
+        mv.addObject("userId", id);
+        mv.addObject("userName", "Utilisateur " + id);
+        return mv;
+    }
+    
+    @URLMapping("/api/info")
+    public String apiInfo(HttpServletRequest req, HttpServletResponse res) {
+        res.setContentType("application/json");
+        return "{\"status\":\"success\",\"message\":\"API Info\"}";
     }
 }
